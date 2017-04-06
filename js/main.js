a0374647fb22930438100a57b66c47e7a07766f7
Aitor Rodríguez <aitor.rodriguez990@gmail.com>
2017-04-06 13:31:11 +0200

    Remove sidebar script file and add code in main.js

--- js/main.js
+++ js/main.js
@@ -12,3 +12,21 @@ $(document).ready(function() {
     SyntaxHighlighter.all();
 
 });
+
+/* --------------------------------------------------------------------------
+  Follow sidebar
+  --------------------------------------------------------------------------- */
+
+$(function() {
+    var $sidebar = $(".sidebar"),
+        $window = $(window),
+        $start = $sidebar.offset().top + 30;
+
+    $window.scroll(function() {
+        if ($window.scrollTop() > $start) {
+            $sidebar.addClass("sidebar-scrolled");
+        } else {
+            $sidebar.removeClass("sidebar-scrolled");
+        }
+    });
+});
