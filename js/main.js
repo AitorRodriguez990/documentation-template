85de980a596e831bf37e1c7fb85c1a007c4b1609
Aitor Rodríguez <aitor.rodriguez990@gmail.com>
2017-04-06 13:29:35 +0200

    Add syntax highlighter

---
+++ js/main.js
@@ -0,0 +1,14 @@
+/* --------------------------------------------------------------------------
+  Initialize
+  --------------------------------------------------------------------------- */
+
+$(document).ready(function() {
+
+    /* Syntax Highlighter
+    --------------------------------------------------------------------------- */
+
+    SyntaxHighlighter.defaults['toolbar'] = false;
+    SyntaxHighlighter.defaults['gutter'] = true;
+    SyntaxHighlighter.all();
+
+});
